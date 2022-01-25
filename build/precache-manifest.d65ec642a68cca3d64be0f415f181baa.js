411c6cf0143894d12d3b3f42f4d7711445ecf94d
RSKarthikKumar <nikhil01karthik@gmail.com>
2022-01-25 13:51:40 +0530

    create build

---
+++ build/precache-manifest.d65ec642a68cca3d64be0f415f181baa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3250c61dfbf1260450d8a230c2c764f",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "d190f50b0386c9b5ba95",
+    "url": "/keeper-app/static/js/2.8804de4b.chunk.js"
+  },
+  {
+    "revision": "08fc233413b1423f3dc2",
+    "url": "/keeper-app/static/js/main.bbcd423e.chunk.js"
+  },
+  {
+    "revision": "fb7d45a05bf0dfe0fe92",
+    "url": "/keeper-app/static/js/runtime-main.42e7a143.js"
+  }
+]);
